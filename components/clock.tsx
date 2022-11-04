c5c576d88bf68f4e0c6df28c48b470e7947c5cb6
Bartek Paczesny <bartek@paczesny.pl>
2022-11-04 17:11:26 +0100

    Fix: changed time format to 12 hr

--- components/clock.tsx
+++ components/clock.tsx
@@ -6,19 +6,21 @@ type Props = {
 };
 
 function Clock({ clock }: Props) {
-    const [time, setTime] = useState<any>([]);
-    const [date, setDate] = useState<any>([]);
+    const [time, setTime] = useState<string>();
+    const [date, setDate] = useState<string>();
     const greetings = ["Dzień dobry", "Dobry wieczór"];
 
     useEffect(() => {
         const interval = setInterval(() => {
             // format time to: 14:06
             setTime(
-                new Date().toLocaleTimeString("pl-PL", {
-                    hour: "numeric",
-                    minute: "numeric",
-                    hour12: false,
-                })
+                new Date()
+                    .toLocaleTimeString("pl-PL", {
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: true,
+                    })
+                    .split(" ")[0]
             );
             // format date to: Monday, November 1, 2022
             setDate(
@@ -33,37 +35,40 @@ function Clock({ clock }: Props) {
         return () => clearInterval(interval);
     }, []);
 
-    return (
-        time && (
-            <Transition
-                show={clock}
-                enter="transition duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="transition duration-300"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-                className={"w-full"}
-            >
-                <section className="w-full text-gray-200 flex flex-col items-center justify-center backdrop-blur-md bg-gray-800/40 bg-opacity-50 shadow-[0_0_3px_1px] shadow-gray-300 px-6 pt-4 rounded-3xl">
-                    <h1 className="font-semibold text-7xl text-start">
-                        {/* display the current time */}
-                        {time}
-                    </h1>
-                    <h2 className="text-2xl">
-                        {/* display the current date */}
-                        {date}
-                    </h2>
-                    <h2 className="text-3xl font-semibold py-8">
-                        {
-                            // display the current greeting
-                            greetings[new Date().getHours() < 12 ? 0 : 1]
-                        }
-                        , Bartek
-                    </h2>
-                </section>
-            </Transition>
-        )
+    return time ? (
+        <Transition
+            show={clock}
+            enter="transition duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition duration-300"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+            className={"w-full"}
+        >
+            <section className="w-full text-gray-200 flex flex-col items-center justify-center backdrop-blur-md bg-gray-800/40 bg-opacity-50 shadow-[0_0_3px_1px] shadow-gray-300 px-6 pt-4 rounded-3xl">
+                <h1 className="font-semibold text-7xl text-start">
+                    {/* display the current time */}
+                    {time}
+                    <span className="text-4xl">
+                        {new Date().getHours() < 12 ? "AM" : "PM"}
+                    </span>
+                </h1>
+                <h2 className="text-2xl">
+                    {/* display the current date */}
+                    {date}
+                </h2>
+                <h2 className="text-3xl font-semibold py-8">
+                    {
+                        // display the current greeting
+                        greetings[new Date().getHours() < 12 ? 0 : 1]
+                    }
+                    , Bartek
+                </h2>
+            </section>
+        </Transition>
+    ) : (
+        <div></div>
     );
 }
 
